c454d03fd2f7121f5db7d8bbfdb56d6d81a32637
SayedBenSlimene <sayedbenslimane@gmail.com>
2024-08-27 16:25:37 +0100

    Workflow devops

---
+++ src/main/java/esprit/tn/revexamenpthologies/controllers/ActeController.java
@@ -0,0 +1,27 @@
+package esprit.tn.revexamenpthologies.controllers;
+
+import esprit.tn.revexamenpthologies.entities.Acte;
+import esprit.tn.revexamenpthologies.services.IActeService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.websocket.server.PathParam;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "acte Management")
+@AllArgsConstructor
+@RequestMapping("/acte")
+@RestController
+public class ActeController {
+//test1234dfseee
+     //saszeczerfzerf
+//saszeczerfzerf
+     @Autowired
+     IActeService iActeService;
+
+     @GetMapping("/AjoutActe/{Id}")
+     public float calculerFacture(@PathVariable("Id") String Id){
+        return iActeService.calculerFacture(Id);
+     }
+     
+}
