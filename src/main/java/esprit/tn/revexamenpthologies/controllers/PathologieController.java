c91ee447623acc0c201b08adface030266b8e318
SayedBenSlimene <sayedbenslimane@gmail.com>
2024-08-30 22:49:47 +0100

    new commit

--- src/main/java/esprit/tn/revexamenpthologies/controllers/PathologieController.java
+++ src/main/java/esprit/tn/revexamenpthologies/controllers/PathologieController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/pathologie")
 @RestController
 public class PathologieController {
-
+//fgvbfgbfgbfgb
      @Autowired
      IPathologieService iPathologieService;
 
